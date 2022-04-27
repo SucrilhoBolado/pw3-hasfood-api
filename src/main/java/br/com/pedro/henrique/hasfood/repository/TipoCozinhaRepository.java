e5be2c72d7242af4e1bbeff939bbd37a3ebd8a9f
nome <rodrigoFreitas2568@test.com>
2022-04-27 15:01:11 -0300

    27abril

---
+++ src/main/java/br/com/pedro/henrique/hasfood/repository/TipoCozinhaRepository.java
@@ -0,0 +1,9 @@
+package br.com.pedro.henrique.hasfood.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.pedro.henrique.hasfood.entities.TipoCozinha;
+
+public interface TipoCozinhaRepository extends JpaRepository<TipoCozinha, Integer>{
+
+}
